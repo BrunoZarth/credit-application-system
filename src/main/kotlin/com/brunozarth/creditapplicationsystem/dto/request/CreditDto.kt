0f27d1f9f11615abb46092b76188765f3080eee3
BrunoZarth <brunozarth@gmail.com>
2023-04-26 18:50:09 -0300

    implements: DTO

---
+++ src/main/kotlin/com/brunozarth/creditapplicationsystem/dto/request/CreditDto.kt
@@ -0,0 +1,25 @@
+package com.brunozarth.creditapplicationsystem.dto.request
+
+import com.brunozarth.creditapplicationsystem.entity.Credit
+import com.brunozarth.creditapplicationsystem.entity.Customer
+import jakarta.validation.constraints.Future
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotNull
+import java.math.BigDecimal
+import java.time.LocalDate
+
+data class CreditDto(
+    @field:NotNull(message = "Invalid input") val creditValue: BigDecimal,
+    @field:Future val dayFirstOfInstallment: LocalDate,
+    @field:Min(value = 1) @field:Max(value = 48) val numberOfInstallments: Int,
+    @field:NotNull(message = "Invalid input") val customerId: Long
+) {
+
+    fun toEntity(): Credit = Credit(
+        creditValue = this.creditValue,
+        dayFirstInstallment = this.dayFirstOfInstallment,
+        numberOfInstallments = this.numberOfInstallments,
+        customer = Customer(id = this.customerId)
+    )
+}
